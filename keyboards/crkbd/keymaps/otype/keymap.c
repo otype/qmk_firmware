83fc455207d7a768291781aa34c252caef48e4ab
Hans-Gunther Schmidt <hans@otype.de>
2024-12-09 07:49:28 +0100

    Provide different rendering for each side

--- keyboards/crkbd/keymaps/otype/keymap.c
+++ keyboards/crkbd/keymaps/otype/keymap.c
@@ -93,28 +93,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef OLED_ENABLE
-bool oled_task_user() {
+static void render_status(void) {
   oled_set_cursor(0, 1);
 
   switch (get_highest_layer(layer_state)) {
   case _BASE:
-    oled_write_ln_P(PSTR("LAYER> Base"), false);
+    oled_write_ln_P(PSTR("LAYER: Base"), false);
     break;
   case _NUM:
-    oled_write_ln_P(PSTR("LAYER> Nums & Syms"), false);
+    oled_write_ln_P(PSTR("LAYER: Nums & Syms"), false);
     break;
   case _NAV:
-    oled_write_ln_P(PSTR("LAYER> Navigation"), false);
+    oled_write_ln_P(PSTR("LAYER: Navigation"), false);
     break;
   case _CODE:
-    oled_write_ln_P(PSTR("LAYER> Code"), false);
+    oled_write_ln_P(PSTR("LAYER: Code"), false);
     break;
   case _DEV:
-    oled_write_ln_P(PSTR("LAYER> IDE/Dev"), false);
+    oled_write_ln_P(PSTR("LAYER: IDE/Dev"), false);
     break;
   }
+}
 
-  return false;
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+
+    return rotation;
+}
+
+bool oled_task_user() {
+    if (is_keyboard_master()) {
+        render_status();
+    } else {
+        render_logo();
+
+        // Turns on scrolling
+        oled_scroll_left();
+    }
+    return false;
 }
 
 void oled_render_boot(bool bootloader) {
